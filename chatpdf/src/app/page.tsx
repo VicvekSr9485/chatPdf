0330308e0b296248aa85d6166ca64ead2d1df407
VicvekSr9485 <olamideoso65@gmail.com>
2023-09-30 20:29:12 +0100

    feats: add database and orm configs

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -1,7 +1,41 @@
-import Image from 'next/image'
+import { UserButton, auth } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { LogIn } from 'lucide-react'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+  const isAuth = !!userId
   return (
-    <h1 className='text-red-600'>Hello World</h1>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex  items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex">
+          {isAuth && <Button>Go to Chats</Button>}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly answer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (<h1>fileUpload</h1>):(
+              <Link href="/sign-in">
+              
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
